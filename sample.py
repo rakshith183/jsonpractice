ffdb6d09bf85a8d4455d896331ac13ad13ca6662
GS-SAURAV <122368981+GS-SAURAV@users.noreply.github.com>
2023-02-03 17:40:49 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,6 +1,15 @@
 import pandas as pd
 import json 
 
+# .....reading json file named data.json......
 event_data = pd.read_json("data.json")
 
-print(event_data)
+# print(event_data)
+# print(type(event_data))
+# print(event_data.info())
+
+# converting into excel file
+# event_data.to_excel("convert_excel.xlsx", index=False)
+
+# converting into csv file
+event_data.to_csv("convert_csv.csv", index=False)
